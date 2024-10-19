57deace1ad624048bec3428e5f5b7a5339964af1
Lukasz <lkowalik04@gmail.com>
2024-10-20 01:12:51 +0200

    tests: fix all test cases, add more test cases

--- Zadanie01Test.java
+++ Zadanie01Test.java
@@ -1,100 +1,93 @@
 package zadanie01;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Zadanie01Test {
 
-    private Zadanie01 zadanie;
-
-    @BeforeEach
-    void setUp() {
-        zadanie = new Zadanie01();
-    }
-
     @Test
     void testInitialError() {
+        Zadanie01 zadanie = new Zadanie01();
         assertEquals(-2_000_000_000, zadanie.wynik());
     }
 
     @Test
     void testAddition() {
-        // 0101 11111 00 01110 => (-15 + 14) = -1
+        Zadanie01 zadanie = new Zadanie01();
+        int result = -15 + 15;
         int[] bits = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 0,
-                0, 1, 1, 1, 0
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, //-15
+            0, 0, // "+"
+            0, 1, 1, 1, 1 // 15
         };
 
         for (int bit : bits) {
             zadanie.input(bit);
         }
-
-        assertEquals(-1, zadanie.wynik());
+        assertEquals(result, zadanie.wynik());
     }
 
     @Test
     void testSubtraction() {
-        // 0101 11111 01 01110 => (-15 - 14) = -29
+        Zadanie01 zadanie = new Zadanie01();
+        int result = -15 - 15;
         int[] bits = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 1,
-                0, 1, 1, 1, 0
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, //-15
+            0, 1, // "-"
+            0, 1, 1, 1, 1 // 15
         };
 
         for (int bit : bits) {
             zadanie.input(bit);
         }
-
-        assertEquals(-29, zadanie.wynik());
+        assertEquals(result, zadanie.wynik());
     }
 
     @Test
     void testMultiplication() {
-        // 0101 00011 10 00011 => (3 * 3) = 9
+        Zadanie01 zadanie = new Zadanie01();
+        int result = -15 * 15;
         int[] bits = {
-                0, 1, 0, 1,
-                0, 0, 0, 1, 1,
-                1, 0,
-                0, 0, 0, 1, 1
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, //-15
+            1, 0, // "*"
+            0, 1, 1, 1, 1 // 15
         };
 
         for (int bit : bits) {
             zadanie.input(bit);
         }
-
-        assertEquals(9, zadanie.wynik());
+        assertEquals(result, zadanie.wynik());
     }
 
     @Test
     void testDivision() {
-        // 0110 00100 11 00010 => (4 / 2) = 2
+        Zadanie01 zadanie = new Zadanie01();
+        int result = -15 / 15;
         int[] bits = {
-                0, 1, 0, 1,
-                0, 0, 1, 0, 0,
-                1, 1,
-                0, 0, 0, 1, 0
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, //-15
+            1, 1, // "/"
+            0, 1, 1, 1, 1 // 15
         };
 
         for (int bit : bits) {
             zadanie.input(bit);
         }
-
-        assertEquals(2, zadanie.wynik());
+        assertEquals(result, zadanie.wynik());
     }
 
     @Test
     void testErrorForDivisionByZero() {
-        // 0110 00100 11 00000 => (4 / 0) => BLAD
+        Zadanie01 zadanie = new Zadanie01();
         int[] bits = {
-                0, 1, 1, 0,
-                0, 0, 1, 0, 0,
-                1, 1,
-                0, 0, 0, 0, 0
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, //-15
+            1, 1, // "/"
+            0, 0, 0, 0, 0 // 0
         };
 
         for (int bit : bits) {
@@ -106,60 +99,60 @@ public class Zadanie01Test {
 
     @Test
     void testResultCanOnlyBeFetchedOnce() {
-        // 0101 11111 01 01110 => (-15 - 14) = -29
+        Zadanie01 zadanie = new Zadanie01();
+        int result = -15 + 15;
         int[] bits = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 1,
-                0, 1, 1, 1, 0
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, //-15
+            0, 0, // "+"
+            0, 1, 1, 1, 1 // 15
         };
 
         for (int bit : bits) {
             zadanie.input(bit);
         }
-
-        assertEquals(-29, zadanie.wynik());
+        assertEquals(result, zadanie.wynik());
         assertEquals(-2_000_000_000, zadanie.wynik());
     }
 
     @Test
     void testResultOverwrittenByNewInput() {
+        Zadanie01 zadanie = new Zadanie01();
         int[] firstBits = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 0,
-                0, 1, 1, 1, 0
+            1, 1, 1, 1, //15
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //-16383
+            0, 0, // "+"
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //-16383
         };
 
         for (int bit : firstBits) {
             zadanie.input(bit);
         }
 
-        assertEquals(-1, zadanie.wynik());
+        assertEquals(-16383 + -16383, zadanie.wynik());
 
         int[] secondBits = {
-                0, 0, 1, 0,
-                0, 0,
-                0, 0,
-                0, 1
+            1, 1, 1, 1, //15
+            0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //16383
+            0, 1, // "-"
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, //-16382
         };
 
         for (int bit : secondBits) {
             zadanie.input(bit);
         }
 
-        // New result should be 1, old result is overwritten
-        assertEquals(1, zadanie.wynik());
+        assertEquals(16383 + 16382, zadanie.wynik());
     }
 
     @Test
     void testHandlingSmallNumbers() {
-        // 0010 001 00 001 => (1 + 1) = 2
+        Zadanie01 zadanie = new Zadanie01();
         int[] bits = {
-                0, 0, 1, 1,
-                0, 0, 1,
-                0, 0,
-                0, 0, 1
+            0, 0, 1, 0, //15
+            0, 1, //1
+            0, 1, // "-"
+            1, 1, //-1
         };
 
         for (int bit : bits) {
@@ -171,55 +164,28 @@ public class Zadanie01Test {
 
     @Test
     void testHandlingNegativeNumbers() {
-        //  0 - -1
+        Zadanie01 zadanie = new Zadanie01();
         int[] bits = {
-                0, 0, 1, 1,
-                1, 0, 0,
-                0, 1,
-                1, 0, 1
+            0, 0, 1, 0, //15
+            1, 0, // -0
+            1, 0, // "*"
+            1, 1, // -1
         };
 
         for (int bit : bits) {
             zadanie.input(bit);
         }
 
-        assertEquals(1, zadanie.wynik());
-    }
-    @Test
-    void testTwoSequencesBackToBack() {
-        int[] firstSequence = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 0,
-                0, 1, 1, 1, 0
-        };
-
-        for (int bit : firstSequence) {
-            zadanie.input(bit);
-        }
-
-        assertEquals(-1, zadanie.wynik());
-
-        int[] secondSequence = {
-                0, 0, 1, 0,
-                0, 0,
-                0, 0,
-                0, 1
-        };
-
-        for (int bit : secondSequence) {
-            zadanie.input(bit);
-        }
-
-        assertEquals(1, zadanie.wynik());
+        assertEquals(0, zadanie.wynik());
     }
 
     @Test
     void testNotEnoughBits() {
+        Zadanie01 zadanie = new Zadanie01();
         int[] partialSequence = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 0
+            0, 1, 0, 1, //5
+            1, 1, 1, 1, 1, // -15
+            0, 0
         };
 
         for (int bit : partialSequence) {
@@ -230,13 +196,13 @@ public class Zadanie01Test {
 
     @Test
     void testTooManyBits() {
-        // 0101 11111 00 01110 => (-15 + 14) = -1
+        Zadanie01 zadanie = new Zadanie01();
         int[] bits = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 0,
-                0, 1, 1, 1, 0,
-                1, 1, 1
+            0, 1, 0, 1,
+            1, 1, 1, 1, 1,
+            0, 0,
+            0, 1, 1, 1, 0,
+            1, 1, 1
         };
 
         for (int bit : bits) {
@@ -248,17 +214,17 @@ public class Zadanie01Test {
 
     @Test
     void testIncompleteSecondSequence() {
-        // 0101 11111 00 01110 => (-15 + 14) = -1
+        Zadanie01 zadanie = new Zadanie01();
         int[] firstSequence = {
-                0, 1, 0, 1,
-                1, 1, 1, 1, 1,
-                0, 0,
-                0, 1, 1, 1, 0
+            0, 1, 0, 1,
+            1, 1, 1, 1, 1,
+            0, 0,
+            0, 1, 1, 1, 0
         };
 
         int[] incompleteSecondSequence = {
-                0, 0, 1, 0,
-                0, 0
+            0, 0, 1, 0,
+            0, 0
         };
 
         for (int bit : firstSequence) {
@@ -276,58 +242,71 @@ public class Zadanie01Test {
 
     @Test
     void testAdditionOfPositiveNumbers() {
-        // 3 + 3 = 6
-        int[] input = {0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1};
+        Zadanie01 zadanie = new Zadanie01();
+        int[] input = {
+            0, 0, 1, 1, //  3
+            0, 0, 1, //  1
+            1, 0, // "*"
+            1, 1, 1 // "-3"
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
-        assertEquals(6, zadanie.wynik());
-    }
-
-    @Test
-    void testAdditionOfSmallNumbers() {
-        // 1 + 1 = 2
-        int[] input = {0, 0, 1, 0, 0, 1, 0, 0, 0, 1};
-        for (int bit : input) {
-            zadanie.input(bit);
-        }
-        assertEquals(2, zadanie.wynik());
+        assertEquals(-3, zadanie.wynik());
     }
 
     @Test
     void testMultiplicationOfNegativeNumbers() {
-        // -3 * 3 = -9
-        int[] input = {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1};
+        Zadanie01 zadanie = new Zadanie01();
+        int[] input = {
+            0, 0, 1, 1, //3
+            1, 1, 1, // -3
+            1, 0, // "*"
+            0, 0, 1 // 1
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
-        assertEquals(-9, zadanie.wynik());
+        assertEquals(-3, zadanie.wynik());
     }
 
     @Test
     void testDivisionOfNegativeAndPositiveNumbers() {
-        // -3 / 3 = -1
-        int[] input = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1};
+        Zadanie01 zadanie = new Zadanie01();
+        int[] input = {
+            0, 0, 1, 1, //  3
+            1, 1, 1, //  -3
+            1, 1, // "/"
+            1, 0, 1 // -1
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
-        assertEquals(-1, zadanie.wynik());
+        assertEquals(3, zadanie.wynik());
     }
 
     @Test
     void testSubtractionOfNegativeAndPositiveNumbers() {
-        // -3 - 3 = -6
-        int[] input = {0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1};
+        Zadanie01 zadanie = new Zadanie01();
+        int[] input = {
+            0, 0, 1, 1, //3
+            1, 1, 1, //-3
+            1, 0, //"*"
+            1, 0, 1 //-1
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
-        assertEquals(-6, zadanie.wynik());
+        assertEquals(3, zadanie.wynik());
     }
 
     @Test
     void testInvalidLengthForA() {
+        Zadanie01 zadanie = new Zadanie01();
         // Invalid input for part "a"
-        int[] input = {0, 0, 1};
+        int[] input = {
+            0, 0, 1
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
@@ -336,8 +315,12 @@ public class Zadanie01Test {
 
     @Test
     void testInvalidLengthForB() {
+        Zadanie01 zadanie = new Zadanie01();
         // Invalid input for part "b"
-        int[] input = {1, 1, 1, 1, 0, 0, 0};
+        int[] input = {
+            1, 1, 1, 1,
+            0, 0, 0
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
@@ -346,26 +329,30 @@ public class Zadanie01Test {
 
     @Test
     void testTooLongInput() {
+        Zadanie01 zadanie = new Zadanie01();
         // Input sequence too long
-        int[] input = {0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0};
-        for (int bit : input) {
-            zadanie.input(bit);
-        }
-        assertEquals(0, zadanie.wynik());
-    }
+        int[] input = {
+            0, 0, 1, 1, //3
+            1, 1, 1, //-3
+            0, 0, //"+"
+            1, 1, 1, //-3
 
-    @Test
-    void testAdditionOfSmallNumbersAfterMultipleRounds() {
-        // 1 + 1 = 2
-        int[] input = {0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1};
+            0, 0, 1, 0, //2
+            1, 0, // -0
+            0, 1, // "-"
+            0 // will be 1
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
-        assertEquals(2, zadanie.wynik());
+        assertEquals(-6, zadanie.wynik());
+        zadanie.input(1);
+        assertEquals(-1, zadanie.wynik());
     }
 
     @Test
     void testIncompleteData() {
+        Zadanie01 zadanie = new Zadanie01();
         // Incomplete input, insufficient data bits
         int[] input = {0, 0, 1, 1, 0, 1, 1, 1, 0, 1};
         for (int bit : input) {
@@ -376,6 +363,7 @@ public class Zadanie01Test {
 
     @Test
     void testNegativeNumberInFirstByte() {
+        Zadanie01 zadanie = new Zadanie01();
         // Negative first number (-10)
         int[] input = {1, 0, 1, 0, 0, 1, 0, 0, 0, 1};
         for (int bit : input) {
@@ -386,8 +374,18 @@ public class Zadanie01Test {
 
     @Test
     void testMultipleRoundsWithNewData() {
-        // First: 3 + 3 = 6, then -3 + 3 = 0
-        int[] input = {0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+        Zadanie01 zadanie = new Zadanie01();
+        int[] input = {
+            0, 0, 1, 1, //3
+            0, 1, 1, //3
+            0, 0, //"+"
+            0, 1, 1, //3
+
+            0, 0, 1, 1, //3
+            1, 1, 1, //-3
+            0, 0, //"+"
+            0, 1, 1 //3
+        };
         for (int bit : input) {
             zadanie.input(bit);
         }
@@ -395,12 +393,38 @@ public class Zadanie01Test {
     }
 
     @Test
-    void testMultipleRoundsWithTwoInputs() {
-        // First: 3 + 3 = 6, then -3 - 3 = -6
-        int[] input = {0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1};
+    void testAdditionWithLength1Operands() {
+        Zadanie01 zadanie = new Zadanie01();
+        int[] input = {
+            0, 0, 0, 1, // 1
+            0, // 0
+            0, 0, // "+"
+            1 // 0
+        };
+
         for (int bit : input) {
             zadanie.input(bit);
         }
-        assertEquals(-6, zadanie.wynik());
+
+        assertEquals(-2_000_000_000, zadanie.wynik());
+    }
+
+    @Test
+    void testTwoIndependentInstances() {
+        Zadanie01 instance1 = new Zadanie01();
+        Zadanie01 instance2 = new Zadanie01();
+
+        int[] input1 = {0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0};
+        for (int bit : input1) {
+            instance1.input(bit);
+        }
+
+        int[] input2 = {0, 0, 1, 0, 0, 0, 0, 1};
+        for (int bit : input2) {
+            instance2.input(bit);
+        }
+
+        assertEquals(-1, instance1.wynik());
+        assertEquals(-2_000_000_000, instance2.wynik());
     }
 }
